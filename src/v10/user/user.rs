8f54e188526a10b736834c1af5f67b359fe903bb
Luuk van Oijen <lazyluuk.channel@gmail.com>
2026-03-23 22:56:55 +0100

    initial commit

---
+++ src/v10/user/user.rs
@@ -0,0 +1,14 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct User {
+    pub id: String,
+    pub username: String,
+    pub discriminator: String,
+    pub global_name: Option<String>,
+    pub avatar: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub bot: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub system: Option<bool>,
+}
