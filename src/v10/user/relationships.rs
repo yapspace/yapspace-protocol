98e04aa6b89e79f3258975a83a02f04def779a45
Luuk van Oijen <lazyluuk.channel@gmail.com>
2026-03-26 00:21:48 +0100

    create relationship

--- src/v10/user/relationships.rs
+++ src/v10/user/relationships.rs
@@ -1,13 +1,25 @@
 use serde::{Deserialize, Serialize};
+use strum::FromRepr;
 
 use crate::v10::user::PartialUser;
+
+#[derive(FromRepr, Serialize, Deserialize)]
+#[repr(i32)]
+pub enum RelationshipType {
+    None = 0,
+    Friend = 1,
+    Blocked = 2,
+    IncomingRequest = 3,
+    OutgoingRequest = 4,
+    Implicit = 5,
+}
 
 #[derive(Serialize, Deserialize)]
 pub struct Relationship {
     /// The ID of the target user
     pub id: String,
     /// The type of relationship
-    pub r#type: i64,
+    pub r#type: RelationshipType,
     /// The target user
     pub user: PartialUser,
     /// The nickname of the user in this relationship (1-32 characters)
@@ -29,4 +41,15 @@ pub struct Relationship {
 pub struct SendFriendRequest {
     pub username: String,
     pub discriminator: Option<String>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct CreateRelationship {
+    /// If left out, will default to -1
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub r#type: Option<RelationshipType>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub from_friend_suggestion: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    confirm_stranger_request: Option<bool>,
 }
