dacecba7d30a7ec972c4471fa21d7fa8fbf012b8
Luuk van Oijen <lazyluuk.channel@gmail.com>
2026-03-25 20:53:48 +0100

    message update

--- src/v10/channel/message.rs
+++ src/v10/channel/message.rs
@@ -1,11 +1,26 @@
 use serde::{Deserialize, Serialize};
 
+use crate::v10::user::User;
+
+#[derive(Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum MessageAuthor {
+    Author(User),
+    Webhook(WebhookAuthor),
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct WebhookAuthor {
+    pub id: String,
+    pub username: String,
+}
+
 #[derive(Serialize, Deserialize)]
 pub struct Message {
     pub id: String,
     pub channel_id: i64,
+    pub author: MessageAuthor,
     pub content: String,
-    pub author_id: i64,
-    pub timestamp: std::time::SystemTime,
-    pub edited_timestamp: Option<std::time::SystemTime>,
+    pub timestamp: String,
+    pub edited_timestamp: Option<String>,
 }
